db68f78014a29381582eee2c417e645872bc3555
NicoHood <git@nicohood.de>
2018-01-16 18:51:45 +0100

    Simplified necapi

--- src/IRL_NecAPI.h
+++ src/IRL_NecAPI.h
@@ -31,7 +31,7 @@ THE SOFTWARE.
 //==============================================================================
 
 typedef void(*NecEventCallback)(void);
-#define NEC_API_PRESS_TIMEOUT (500UL * 1000UL)
+#define NEC_API_PRESS_TIMEOUT (500UL * 1000UL) // 0.5 seconds
 
 template<const NecEventCallback callback, const uint16_t address = 0x0000>
 class CNecAPI : public CNec
@@ -40,13 +40,11 @@ public:
     // User API to access library data
     inline void read(void);
     inline uint8_t command(void);
-    inline uint8_t pressCount(void);
-    inline uint8_t holdCount(const uint8_t debounce = 0);
+    inline uint8_t count(void);
+    inline uint8_t duration(bool raw = false);
+    inline uint8_t released(bool samebutton = false);
     inline constexpr uint32_t getTimeout(void);
-    inline uint8_t pressTimeout(void);
     inline uint32_t nextTimeout(void);
-    inline bool releaseButton (void);
-    inline void reset(void);
 
 protected:
     // Differenciate between timeout types
@@ -148,58 +146,56 @@ void CNecAPI<callback, address>::read(void) {
 template<const NecEventCallback callback, const uint16_t address>
 uint8_t CNecAPI<callback, address>::command(void)
 {
-  return lastCommand;
+    return lastCommand;
 }
 
-
-// Returns the current button press streak.
-// How many times have you pressed again the same button?
+// Number of repeating button presses in a row
 template<const NecEventCallback callback, const uint16_t address>
-uint8_t CNecAPI<callback, address>::pressCount(void)
+uint8_t CNecAPI<callback, address>::count(void)
 {
-  return lastPressCount;
+    return lastPressCount;
 }
 
-
-// Counts how long a button press streak has been holding down.
-// A debounce input param can be used to only count every Xth time.
+// Duration (count) how long the current button press was held down.
+// Pass true to also recognize keyup events
 template<const NecEventCallback callback, const uint16_t address>
-uint8_t CNecAPI<callback, address>::holdCount(const uint8_t debounce )
+uint8_t CNecAPI<callback, address>::duration(bool raw)
 {
-  // Only recognize the actual keydown event
-  if (NecTimeoutType == NO_TIMEOUT)
-  {
-    // No debounce desired (avoid zero division)
-    if (!debounce) {
-      return 1 + lastHoldCount;
+    // Only recognize the actual keydown event
+    if (NecTimeoutType == NO_TIMEOUT || raw) // TODO reorder?
+    {
+        return 1 + lastHoldCount;
     }
-
-    // Only recognize every xth event
-    if (!(lastHoldCount % debounce)) {
-      return (lastHoldCount / debounce) + 1;
-    }
-  }
-  return 0;
+    return 0;
 }
 
+// True when the button is released:
+// 1. Timeout of press series
+// 2. Next press, new button
+// 3. Next press, same button
+// `--> pass true as parameter,
+//      useful when measuring idential or single button press durations
+// False when the button is held down:
+// 1. Initial button press
+// 2. Holding button down
+// 3. Renewed button press
+// Usually you want to use timeout() to check if the series ends
+template<const NecEventCallback callback, const uint16_t address>
+uint8_t CNecAPI<callback, address>::released(bool samebutton)
+{
+    if (NecTimeoutType == TIMEOUT || NecTimeoutType == NEW_BUTTON) {
+        return 1 + lastHoldCount;
+    }
+    if (samebutton && NecTimeoutType == NEXT_BUTTON) {
+        return 1 + lastHoldCount;
+    }
+    return 0;
+}
 
 template<const NecEventCallback callback, const uint16_t address>
 constexpr uint32_t CNecAPI<callback, address>::getTimeout(void) {
     return NEC_API_PRESS_TIMEOUT;
 }
-
-
-// Check if a key was released (via timeout or another key got pressed).
-// Return how often the key was pressed.
-template<const NecEventCallback callback, const uint16_t address>
-uint8_t CNecAPI<callback, address>::pressTimeout(void)
-{
-  if (NecTimeoutType == TIMEOUT || NecTimeoutType == NEW_BUTTON) {
-    return lastPressCount;
-  }
-  return 0;
-}
-
 
 // Return when the next timeout triggers.
 // Zero means it already timed out.
@@ -214,34 +210,4 @@ uint32_t CNecAPI<callback, address>::nextTimeout(void)
     }
 
     return timeout - time;
-}
-
-
-// Triggers when the button is released.
-// Anything else than a normal press indicates the key was released.
-// This occurs on a timeout, new button, same button press.
-// In most cases pressTimeout makes more sense.
-template<const NecEventCallback callback, const uint16_t address>
-bool CNecAPI<callback, address>::releaseButton(void)
-{
-  if (NecTimeoutType != NO_TIMEOUT) {
-    return true;
-  }
-  return false;
-}
-
-
-// Reset the button press and hold count.
-// Attention: No release/timeout event will trigger!
-// This is important if you want to end a chain,
-// which starts again with the next press.
-// Differenciate between 1 or 2 presses is a good usecase.
-// Hold a button -> event A triggers
-// Hold the button again -> event B triggers
-// If A (with a longer hold count) was already triggered, reset the API
-template<const NecEventCallback callback, const uint16_t address>
-void CNecAPI<callback, address>::reset(void)
-{
-  lastPressCount = 0;
-  lastHoldCount = 0;
 }
